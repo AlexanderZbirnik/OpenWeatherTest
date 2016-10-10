9726a595d478cf93eaacac5cca992e779b5205cf
Alex Zbirnik <alxzorg@gmail.com>
2016-10-10 14:59:58 +0300

    Create project

---
+++ OpenWeatherTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenWeatherTest
+//
+//  Created by Alex Zbirnik on 10.10.16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenWeatherTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OpenWeatherTest
+//
+//  Created by Alex Zbirnik on 10.10.16.
+//  Copyright © 2016 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
